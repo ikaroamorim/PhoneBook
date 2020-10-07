f9bfa47cadfcb8ef023310eb0e5654c78009e500
ikaroamorim <ikaro.amorim@gmail.com>
2020-10-06 22:08:19 -0300

    :ambulance:  Installed Html Plugin Webpack

--- src/scripts/usersDel.js
+++ src/scripts/usersDel.js
@@ -1,5 +1,4 @@
 const userDel = (id) =>{
     dataDelete(id)
     renderTable()
-
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     mode: "development",
+    devServer: {
+        open: true,
+        contentBase: "dist"
+    },
     entry: "./src/scripts/main.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html"
+    })]
 }
